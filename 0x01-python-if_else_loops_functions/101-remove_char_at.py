3465e88317a71e0f8042ee6401b3caefee795493
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-08 23:35:22 +0000

    fixed indentation in file

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(str, n):
     string1 = ""
     for num1 in range(0, len(str)):
         if num1 != n:
-             string1 += str[num1]
+            string1 += str[num1]
     return string1
