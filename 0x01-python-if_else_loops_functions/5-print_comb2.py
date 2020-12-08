daef74e3a8b02c1899121e4c73a14027f888be26
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-08 19:39:56 +0000

    added file for testing

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(00, 100):
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end='')
     if i == 99:
         print(i)
-        break
-    print(i, end='')
-    if i != 99:
-        print(", ", end='')
