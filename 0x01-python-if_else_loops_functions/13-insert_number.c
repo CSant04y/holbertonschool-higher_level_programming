4273dcdd22beb3577c69d293871b77e14d52e67d
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-08 22:51:52 +0000

    added some changed to file

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * insert_node - inserting a node at a certian point
+ * @head: this is head of the list
+ * @number: this is the number that you feed in
+ * return: the linked list
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	(void) head;
+	(void) number;
+	return ('\0');
+}
