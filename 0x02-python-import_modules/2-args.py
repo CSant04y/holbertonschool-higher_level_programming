31304367391374fd7e2c368736875ab296ed6e82
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-10 18:41:49 +0000

    added fixes to file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,9 @@ if __name__ == "__main__":
     le = (len(sys.argv)-1)
     if le == 0:
         print("{:d}: arguments.".format(0, sys.argv[le]))
-        if le == 1:
-            print("{:d} argument:\n{:d}: {} ".format(le, le, sys.argv[le]))
-        if le > 1:
-                print("{:d} arguments:".format(le))
-
-        for i in range(1, (le + 1)):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    if le == 1:
+        print("{:d} argument:\n{:d}: {} ".format(le, le, sys.argv[le]))
+    if le > 1:
+        print("{:d} arguments:".format(le))
+    for i in range(1, (le + 1)):
+        print("{:d}: {}".format(i, sys.argv[i]))
