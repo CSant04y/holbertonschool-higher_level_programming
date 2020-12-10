321ebdc8d78e867a01aa3b2bcb04bafe9198f694
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-10 18:51:43 +0000

    added chnaged to file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     le = (len(sys.argv)-1)
+
     if le == 0:
         print("{:d}: arguments.".format(0, sys.argv[le]))
+
     if le == 1:
-        print("{:d} argument:\n{:d}: {} ".format(le, le, sys.argv[le]))
+        print("{:d} argument:".format(1))
+
     if le > 1:
         print("{:d} arguments:".format(le))
+
     for i in range(1, (le + 1)):
         print("{:d}: {}".format(i, sys.argv[i]))
