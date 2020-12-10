6400561370b442bc41eee80db99f06a2cc1b877b
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-10 18:30:46 +0000

    added files to repo

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
+if __name__ == “__main__”:
+    import sys
 
-length = len(sys.argv)
-num1 = 0
+    length = len(sys.argv)
+    num1 = 0
 
-for i in range(1, length):
-    num1 += int(sys.argv[i])
-print("{:d}".format(num1))
+    for i in range(1, length):
+        num1 += int(sys.argv[i])
+    print("{:d}".format(num1))
