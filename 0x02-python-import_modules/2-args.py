700080d0bd557169940e5165c0d879bb1153b41f
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-10 17:34:07 +0000

    added file to test

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+length = (len(sys.argv)-1)
+
+if length == 0:
+    print("{:d}: arguments.".format(0, sys.argv[length]))
+if length == 1:
+    print("{:d} argument:\n{:d}: {} ".format(length, length, sys.argv[length]))
+if length > 1:
+    print("{:d} arguments:".format(length))
+
+    for i in range(1, (length + 1)):
+        print("{:d}: {}".format(i, sys.argv[i]))
