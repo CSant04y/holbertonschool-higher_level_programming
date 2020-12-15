cdc1d5e62041a1811dc4126689eaf187ab88ffb8
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-15 23:36:27 +0000

    added file to test

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,7 @@ def new_in_list(my_list, idx, element):
     if not my_list:
         return None
     a = my_list[:]
-    if idx > len(my_list) and idx < 0:
-        return a
-    else:
-        a[idx] = element
+    if idx > len(my_list) - 1 or idx < 0:
+        return my_list
+    a[idx] = element
     return a
