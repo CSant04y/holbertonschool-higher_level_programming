e68b8140eb4a3d5f073a1cc83de0e6c1c1c5d3d3
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-15 19:45:07 +0000

    replaced list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,6 @@
 def replace_in_list(my_list, idx, element):
     for i in range(len(my_list)):
         if i == idx:
-            my_list[i] = element;
+            my_list[i] = element
             break
     return my_list
