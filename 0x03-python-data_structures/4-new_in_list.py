d9b4322f0f3480851feb38629ee83b9405a6b9bb
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-15 23:15:35 +0000

    added file with dual check

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if not my_list:
         return None
     a = my_list[:]
-    if idx > len(my_list):
+    if idx > len(my_list) and idx < 0:
         return a
     else:
         a[idx] = element
