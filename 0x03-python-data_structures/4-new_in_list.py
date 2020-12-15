4ab2fdd08b53c5ccad5fdc6b5c67bdde21b8ff52
CSant04y <esquivelcarlos27893@gmail.com>
2020-12-15 23:14:15 +0000

    added else statment

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     if not my_list:
         return None
-
     a = my_list[:]
     if idx > len(my_list):
         return a
-    a[idx] = element
+    else:
+        a[idx] = element
     return a
