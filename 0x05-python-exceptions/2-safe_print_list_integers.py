1d244be25cd2c10d89f8da06ef6d8cc99509fd65
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-04 20:08:04 +0000

    added files to test

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    try:
+        for itr in range(0, x):
+            if type(my_list[itr]) is not int:
+                continue
+            print("{:d}".format(my_list[itr]), end='')
+            num += 1
+        print()
+        return num
+    except IndexError:
+        raise
+        return num
