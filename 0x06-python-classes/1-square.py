adceecdc722a83f6fe51f1750a2b6051ceb69a54
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-05 23:41:40 +0000

    added initilized class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This initilizes the Class Square"""
+
+
+class Square:
+    """This class Instantates with size (no type/value verification)"""
+
+    def __init__(self, size):
+        self.__size = size
