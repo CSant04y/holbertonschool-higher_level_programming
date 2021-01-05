6e3cd075a82759425be748f127703e4bfb6f4f20
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-05 23:27:31 +0000

    added README file and 0-main.py and 0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""The purpose of this module is to create an empty class called Square"""
+
+
+class Square:
+    """This is the empty class called Square that is defined"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+# 0x06-python-classes
+This project goes over the use of classes and object in python.
+OOP(Object-Oriented programing) is coverd as well as the differnt ways to use them.
