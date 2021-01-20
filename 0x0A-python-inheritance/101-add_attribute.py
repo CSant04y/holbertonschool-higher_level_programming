d0ceb9ce0161e640b2231aa6feb782a458422e76
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-20 01:37:33 +0000

    added chnages

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""defines function that adds a new attribute to an object if it’s possible""
+"""defines function to check if you can add attribute"""
 
 
 def add_attribute(obj, name, value):
-    """checks to see if attribute can be added"""
+    """checks to see if you can add atrribute"""
     if '__dict__' not in dir(obj):
         raise TypeError("can't add new attribute")
     else:
