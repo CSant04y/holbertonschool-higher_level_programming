d323d0ce59bebfdf3078b017f15b42113a8776c6
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-21 01:59:56 +0000

    added files for test

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""defines Class Student"""
+
+
+class Student():
+    """class Student that that initilizes vars"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes student instances"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary descript for JSON serialization of info"""
+        if type(attrs) is list:
+            dictonaryVal = {}
+
+            for k in self.__dict__:
+                for v in attrs:
+                    if k == v:
+                        dictonaryVal[k] = self.__dict__[k]
+            return dictonaryVal
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            if key == "first_name":
+                self.first_name = json[key]
+            if key == "last_name":
+                self.last_name = json[key]
+            if key == "age":
+                self.age = json[key]
