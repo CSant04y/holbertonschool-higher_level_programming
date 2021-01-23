e7a0d8e047cf27f32ecbcfc99c6e869fb2f39a65
CSant04y <esquivelcarlos27893@gmail.com>
2021-01-23 22:00:26 +0000

    added class that makes dictonary into json stringr

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,18 +2,35 @@
 """[This is the Base class]
 """
 
+
+import json
+
+
 class Base:
-    """[This is base class
+    """[This is base class]
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """[summary]
 
         Args:
-            id ([type], optional): [description]. Defaults to None.
+            id ([int]): Defaults to None.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """[This is a static method that is to turn a dictornary
+        to json string]
+
+        Args:
+            list_dictionaries ([List]): [This is a list of dictonaries]
+        """
+        if list_dictionaries is None or 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
