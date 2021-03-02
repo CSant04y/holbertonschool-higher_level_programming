bd4d6779669b51abc955c4613e2ea0924deead2d
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-02 20:24:44 +0000

    added some chnages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Script to list all privilges of MySQL users user_0d_1 and user_0d_2
+-- This Script Shows all granted permissions for users listed
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
