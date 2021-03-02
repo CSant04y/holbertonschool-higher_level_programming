29892764a9f0c60718492c54808d168e007b1b2c
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-02 21:29:35 +0000

    added version to test in checker

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script that creates the MySQL server user user_0d_1
 -- This script checks to see if the USER exits and sets a password and give all priviliges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILIGES *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILIGES * . * TO 'user_0d_1'@'localhost';
