aff0a2d7754ea70e567c41251e0252089a27cf76
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-22 22:45:18 +0000

    chanegd specifier

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-// This script prints "My number: <first argument converted in integer>"
-
 if (process.argv[2] && Number(process.argv[2])) {
-  console.log('My number: %d2f', process.argv[2]);
+  console.log('My number: %d', process.argv[2]);
 } else {
   console.log('Not a number');
 }
