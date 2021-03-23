e6e0dea24f94572b5c5ee473a19358ecfa80460c
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-23 21:10:29 +0000

    addded file

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ exports.nbOccurences = function (list, searchElement) {
 
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-	count += 1;
+      count += 1;
     }
   }
   return count;
