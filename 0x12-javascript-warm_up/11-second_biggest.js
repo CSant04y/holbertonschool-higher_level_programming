4329e4c4e4cf870264260bdf2eda3a807ba71374
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-23 00:31:34 +0000

    added file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const num = process.argv.splice(2);
+
+if (num.length === 0 || num.length === 1) {
+  console.log(0);
+
+} else {
+  let i = 0;
+  let max = 0;
+  let result = 0;
+  if (Number(num[0]) > Number(num[1])) {
+    max = num[0];
+    result = num[1];
+  } else {
+    max = num[1];
+    result = num[0];
+  }
+  for (i = 2; i < num.length; i++) {
+    if (Number(num[i]) >= Number(max)) {
+      result = max;
+      max = num[i];
+    } else if (Number(num[i]) >= Number(result)) {
+      result = num[i];
+    }
+  }
+  console.log(result);
+}
