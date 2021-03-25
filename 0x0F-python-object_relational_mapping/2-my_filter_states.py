64d43ecc7edba360998581ebf6037921d9258838
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-25 20:35:40 +0000

    used format to insert value at argv[index]

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""[script that lists all states with a name starting
+    with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+
+# This makes a connection to the MySQL database
+db = MySQLdb.connect(
+    host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+"""
+ This is a cursor object which allows use to work through
+  the same connection in multiple work environments
+"""
+cur = db.cursor()
+
+cur.execute("SELECT id, name FROM states WHERE\
+    name LIKE '{}' ORDER BY states.id ASC".format(argv[4]))
+# After Select statemtent you use the Fetch all at once method
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+# This is cleanup where all open cursors and databases are closed
+
+cur.close()
+db.close()
