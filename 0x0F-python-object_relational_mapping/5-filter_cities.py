3b26f25da3dac0851c97306c72f813af7a476762
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-26 18:15:16 +0000

    added file

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-
     # This makes a connection to the MySQL database
     db = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
@@ -16,15 +15,15 @@ if __name__ == "__main__":
       the same connection in multiple work environments
     """
     cur = db.cursor()
-    
+
     cur.execute("""SELECT cities.name FROM\
         cities INNER JOIN states ON cities.state_id = states.id\
         WHERE states.name = '{}'\
         ORDER BY cities.id ASC""".format(argv[4]))
     # After Select statemtent you use the Fetch all at once method
-    
+
     rows = cur.fetchall()
-    
+
     for i, row in enumerate(rows, start=0):
         if i != 0:
             print(", ", end="")
