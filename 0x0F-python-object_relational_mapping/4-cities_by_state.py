d7202b333e8f6de82af79210c8539e52dd2cbe66
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-26 18:11:32 +0000

    added file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-
     # This makes a connection to the MySQL database
     db = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
@@ -16,17 +15,17 @@ if __name__ == "__main__":
       the same connection in multiple work environments
     """
     cur = db.cursor()
-    
+
     cur.execute("SELECT cities.id, cities.name, states.name FROM\
         cities INNER JOIN states ON cities.state_id = states.id;")
     # After Select statemtent you use the Fetch all at once method
-    
+
     rows = cur.fetchall()
-    
+
     for row in rows:
         print(row)
-    
+
     # This is cleanup where all open cursors and databases are closed
-    
+
     cur.close()
     db.close()
