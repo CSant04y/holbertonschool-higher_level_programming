c38f2e9c5ccf86feec3b4d64cf8e62b740a45483
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-27 14:46:53 +0000

    took port off

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     # This makes a connection to the MySQL database
     db = MySQLdb.connect(
-        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     """
      This is a cursor object which allows use to work through
       the same connection in multiple work environments
