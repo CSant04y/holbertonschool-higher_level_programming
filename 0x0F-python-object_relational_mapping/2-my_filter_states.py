762c3ecfa68e387a42ce7afd4a240a35a4147418
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-27 14:52:37 +0000

    fiex pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-
     # This makes a connection to the MySQL database
     db = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
