cf3b83e4057650739eebf2d869f387df40ecaadf
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-27 14:44:38 +0000

    Added some chnages

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,25 +7,24 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
+    # This makes a connection to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    """
+     This is a cursor object which allows use to work through
+      the same connection in multiple work environments
+    """
+    cur = db.cursor()
 
-# This makes a connection to the MySQL database
-db = MySQLdb.connect(
-    host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-"""
- This is a cursor object which allows use to work through
-  the same connection in multiple work environments
-"""
-cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    # After Select statemtent you use the Fetch all at once method
 
-cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
-# After Select statemtent you use the Fetch all at once method
+    rows = cur.fetchall()
 
-rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-for row in rows:
-    print(row)
+    # This is cleanup where all open cursors and databases are closed
 
-# This is cleanup where all open cursors and databases are closed
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
