926a986eaf36d74f6cd64337f49a937e3153ee7a
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-27 21:11:09 +0000

    added file

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,10 +12,9 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    state_name = (State(id=6, name='Louisiana'))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    state_name = (State(name='Louisiana'))
     session.add(state_name)
     session.commit()
     print("{}".format(state_name.id))
