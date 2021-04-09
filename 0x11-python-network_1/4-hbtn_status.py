0a6ca51d4fcdcc5ff3af668d6845b8ff5d507203
CSant04y <esquivelcarlos27893@gmail.com>
2021-04-09 23:17:27 +0000

    moved import

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """[Python script that fetches https://intranet.hbtn.io/status]
 """
+import requests
 
 if __name__ == "__main__":
-    import requests
 
     r = requests.get('https://intranet.hbtn.io/status')
     content = r.text
