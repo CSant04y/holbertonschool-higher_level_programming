45092c943e8357fa26827731870716dce3523790
CSant04y <esquivelcarlos27893@gmail.com>
2021-04-09 23:24:14 +0000

    made file executable

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""[Python script that fetches https://intranet.hbtn.io/status]
-"""
+"""[Python script that fetches https://intranet.hbtn.io/status]"""
 import requests
 
 if __name__ == "__main__":
