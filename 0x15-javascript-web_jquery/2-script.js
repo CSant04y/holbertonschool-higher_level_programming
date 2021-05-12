9b1b18acf5ea7972527d6f5a18d427b9355f21fd
CSant04y <esquivelcarlos27893@gmail.com>
2021-05-12 19:08:19 +0000

    added files with javascript

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// This updates the color of the header to #FF0000
+// when the user clicks DIV#red_header
+$('DIV#red_header').click(function(){
+    $('header').css({'color': 'red'});
+});
---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// This adds a class to header element
+// on the event DIV#red_header
+$('DIV#red_header').click(function(){
+    $('header').addClass('red')
+});
