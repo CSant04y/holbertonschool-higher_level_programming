008760a07220af2650a18e9491e7bfd21e4399db
CSant04y <esquivelcarlos27893@gmail.com>
2021-05-12 18:15:14 +0000

    added have script file as well as test files

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// script to update the text color of the HTMLtag header to #FF0000
+// using document.querySelector as the way to do it
+const header = document.querySelector('header');
+header.style = 'color: #FF0000;';
