3efa6cba00f8445f5d96d8e2f6f02e225cc8f86f
CSant04y <esquivelcarlos27893@gmail.com>
2021-05-12 19:29:49 +0000

    added javascript

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+// This toggles the class of an HTML header tag
+// When the user touches the header it toggles the color from green to red
+$('DIV#toggle_header').click(function () {
+    if ($('header').hasClass('green')){
+        $('header').toggleClass('red');
+    } else {
+        $('header').toggleClass('green');
+    }
+});
